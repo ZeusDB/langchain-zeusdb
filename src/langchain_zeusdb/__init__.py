35f71dff8d0403073294662e99b0dbc7914d8888
doubleinfinity <6169958+doubleinfinity@users.noreply.github.com>
2025-08-19 00:53:37 +1000

    Initial commit

--- src/langchain_zeusdb/__init__.py
+++ src/langchain_zeusdb/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from langchain-zeusdb!"
+"""LangChain integration for ZeusDB vector database."""
+
+from .vectorstores import AsyncZeusDBVectorStore, ZeusDBVectorStore
+
+__version__ = "0.1.0"
+__all__ = ["ZeusDBVectorStore", "AsyncZeusDBVectorStore"]
